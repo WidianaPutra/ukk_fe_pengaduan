f9d59631680e80e9b030d4a562c43d13de52064b
= <=>
2026-01-20 08:37:58 +0800

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
 import PsBadge from "@/components/Ps/PsBedge";
 import PsButton from "@/components/Ps/PsButton";
 import PsInput from "@/components/Ps/PsInput";
-import PsSVG from "@/components/Ps/PsSVG";
 import PsTextArea from "@/components/Ps/PsTextArea";
 
 export default function Home() {
   return (
     <div className="px-3">
       <h1 className="">Hallo, World</h1>
-      <PsButton variant="primary">Acumala</PsButton>
+      <PsButton>Acumala</PsButton>
       {/*  */}
       <PsInput label="Testing" error="testing testing" />
       <PsTextArea fullHegiht={true} fullWidth={true} label="Testing" />
       <PsBadge>Hallo, World</PsBadge>
-      {/*  */}
-      <PsSVG name="mail" />
     </div>
   );
 }
